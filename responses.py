3fa1f8179161783901edf250d0c45d97f539b4f7
kevinsmith115 <kevindsmith115@gmail.com>
2023-01-24 17:20:37 -0600

    response naming update

--- responses.py
+++ responses.py
@@ -7,11 +7,16 @@ def get_response(message: str) -> str:
     if p_message == 'hello':
         return 'Hey there!'
 
-    if p_message == 'roll':
-        return str(random.randint(1, 6))
+    if p_message == 'roll d6':
+        return "Result: " + str(random.randint(1, 6)) + " :game_die:"
 
     if p_message == '!help':
         return '`This is a help message that you can modify.`'
 
     if p_message == '!commands':
-        return "The commands are \nHello \nRoll \n!Help \n!Commands"
+        return "The commands are \nHello \nRoll d6 /n Roll d20 \n!Help \n!Commands"
+
+    if p_message == 'roll d20':
+        return "Result: " + str(random.randint(1, 20)) + " :game_die:"
+    
+   
